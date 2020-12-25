c0264b07bbb0c6d39b5968aade3fac6dbd34c0ff
y-i-t-m <58217100+y-i-t-m@users.noreply.github.com>
2020-12-25 16:59:24 +0900

    エントリーファイルにモジュール読み込みの記述

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,4 +1,12 @@
+// @ts-nocheck
+import('what-input');
+import('picturefill');
+import objectFitImages from 'object-fit-images';
+import stickyFill from "./modules/stickyfill";
+
 // イベント実行
 document.addEventListener("DOMContentLoaded", () => {
+stickyFill();
+objectFitImages();
 
 }, false);
